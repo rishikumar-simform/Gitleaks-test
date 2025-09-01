325e7e33b37e1fb2773b77cc1854daf1fa1d7980
rishikumar-simform <rishikumar.vachhani@simformsolutions.com>
2025-09-01 18:53:52 +0530

    code update

--- server/demo.js
+++ server/demo.js
@@ -8,3 +8,7 @@ console.log("secrets=5432 ")
 console.log("database database=yourdatabase ")
 console.log("SG.r4nd0mDummYteStKeY1234567890.ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghi")
 console.log("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMjM0NTYiLCJyb2xlIjoiYWRtaW4ifQdBjftJeZ4CVP-mB92K27uhbU")
+
+
+
+console.log("S3_ACCESS_KEY_ID=AKIAXXX ")
