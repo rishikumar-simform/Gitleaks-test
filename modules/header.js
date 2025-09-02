351debb84d940ca8b198c75d0a62d0f428578f85
rishikumar-simform <rishikumar.vachhani@simformsolutions.com>
2025-09-02 11:27:56 +0530

    code update

---
+++ modules/header.js
@@ -0,0 +1,13 @@
+
+console.log("S3_ACCESS_KEY_ID=AKIAXXX ")
+const AWS_config = {
+    accessKeyId: "AKIAEXAMPLE123456",
+    secretAccessKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+}
+console.log(AWS_config)
+
+const AZURE={
+    AZURE_SUBSCRIPTION_ID: "12345678-90ab-cdef-1234-567890abcdef"
+}
+console.log(AZURE)
+console.log("hello this is the secrets")
