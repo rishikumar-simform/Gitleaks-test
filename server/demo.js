30932b94c2c81b92bdce081484aa2a69c5b873d5
rishikumar-simform <rishikumar.vachhani@simformsolutions.com>
2025-09-02 10:01:15 +0530

    update code

--- server/demo.js
+++ server/demo.js
@@ -16,3 +16,9 @@ const AWS_config = {
     accessKeyId: "AKIAEXAMPLE123456",
     secretAccessKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 }
+console.log(AWS_config)
+
+const AZURE={
+    AZURE_SUBSCRIPTION_ID="12345678-90ab-cdef-1234-567890abcdef"
+}
+console.log(AZURE)
