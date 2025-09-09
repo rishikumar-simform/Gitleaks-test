63eeb97fcdd58f3eb1a62b80e6ebc97a7d91895b
rishikumar-simform <rishikumar.vachhani@simformsolutions.com>
2025-09-09 12:18:03 +0530

    header code updated

--- modules/header.js
+++ modules/header.js
@@ -1,2 +1,4 @@
 console.log("This is a demo file.");
 console.log("Just for testing purposes.");
+// ❌ Example of a hardcoded secret (fake, for testing scanners)
+const AWS_SECRET_KEY = "AKIA1234567890FAKEKEYEXAMPLE";
